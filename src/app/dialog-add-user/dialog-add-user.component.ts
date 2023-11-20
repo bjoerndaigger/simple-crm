034dcd71047162c1c1bf117b0b5abd4db828ed1e
Björn Daigger <124874016+bjoerndaigger@users.noreply.github.com>
2023-11-20 10:44:00 +0100

    user model

--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { User } from 'src/models/user.class';
 
 @Component({
   selector: 'app-dialog-add-user',
   templateUrl: './dialog-add-user.component.html',
-  styleUrls: ['./dialog-add-user.component.scss']
+  styleUrls: ['./dialog-add-user.component.scss'],
 })
 export class DialogAddUserComponent {
+  user = new User();
+  birthDate: Date;
 
+  saveUser() {
+    this.user.birthDate = this.birthDate.getTime();
+    console.log('Current user is', this.user);
+  }
 }
+
+
+
+
+
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
+import { User } from 'src/models/user.class';
 
 @Component({
   selector: 'app-user',
@@ -8,6 +9,8 @@ import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.compo
   styleUrls: ['./user.component.scss'],
 })
 export class UserComponent {
+  user = new User();
+
   constructor(public dialog: MatDialog) {}
 
   openDialog() {
