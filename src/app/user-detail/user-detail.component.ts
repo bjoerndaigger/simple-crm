2014391732885664f5f79958d84f2173881dc6bf
Björn Daigger <124874016+bjoerndaigger@users.noreply.github.com>
2023-12-06 12:32:43 +0100

    get user data

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Firestore, doc, getDoc } from '@angular/fire/firestore';
+import { User } from 'src/models/user.class';
 
 @Component({
   selector: 'app-user-detail',
@@ -8,15 +10,32 @@ import { ActivatedRoute } from '@angular/router';
 })
 
 export class UserDetailComponent {
-
+  firestore: Firestore = inject(Firestore);
   userId = '';
+  user: User = new User();
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.userId = this.route.snapshot.paramMap.get('id');
-      console.log('GOT ID', this.userId);
+    console.log('GOT ID', this.userId);
+    this.getUser(this.userId);
   }
 
+  async getUser(userId: string) {
+    try {
+      const userDoc = await getDoc(doc(this.firestore, 'users', userId));
+      console.log('User data:', userDoc.data());
+      this.user = new User(userDoc.data());
+      console.log('User Object', this.user);
+    } catch (error) {
+      console.error('Error getting document:', error);
+    }
+  }
 }
+
+
+
+
+
 
