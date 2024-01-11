176344dcf4157d68471bf0601ebc359b5006210d
Björn Daigger <124874016+bjoerndaigger@users.noreply.github.com>
2024-01-11 12:10:27 +0100

    register user

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { Firestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,26 +9,62 @@ import { FormControl, Validators } from '@angular/forms';
   styleUrls: ['./sign-up.component.scss']
 })
 export class SignUpComponent {
-  nameFormControl = new FormControl('', [Validators.required]);
+
+  firestore: Firestore = inject(Firestore);
+
   emailFormControl = new FormControl('', [Validators.required, Validators.email]);
-  passwordFormControl = new FormControl('', [Validators.required]);
-  confirmPasswordFormControl = new FormControl('', [Validators.required]);
+  passwordFormControl = new FormControl('', [
+    Validators.required,
+    Validators.pattern(/^.{8,}$/)
+  ]);
+  confirmPasswordFormControl = new FormControl('', [
+    Validators.required,
+    Validators.pattern(/^.{8,}$/),
+    this.passwordsMatchValidator.bind(this)  
+  ]);
 
   register() {
-    if (this.nameFormControl.valid && this.emailFormControl.valid && this.passwordFormControl.valid && this.confirmPasswordFormControl) {
-      const name = this.nameFormControl.value;
+    if (
+      this.emailFormControl.valid &&
+      this.passwordFormControl.valid &&
+      this.confirmPasswordFormControl.valid &&
+      this.passwordFormControl.value === this.confirmPasswordFormControl.value
+    ) {
       const email = this.emailFormControl.value;
       const password = this.passwordFormControl.value;
       const confirmPassword = this.confirmPasswordFormControl.value;
+      const auth = getAuth();
 
-      console.log('Name:', name);
       console.log('E-Mail:', email);
       console.log('Password:', password);
       console.log('Confirm Password:', confirmPassword);
+
+      createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          // Registrierung erfolgreich
+          const user = userCredential.user;
+          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          // Fehlerbehandlung
+        });
     }
   }
 
-  closeSignUpForm(): void {
+  passwordsMatchValidator(control: FormControl) {
+    const password = this.passwordFormControl.value;
+    const confirmPassword = control.value;
+  
+    if (password !== confirmPassword) {
+      return { passwordsNotMatch: true };
+    }
+  
+    return null;
+  }
+
+  closeSignUpForm() {
     location.reload();
   }
 }
